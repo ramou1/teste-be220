1cafea6ced7da166b8c80d8d0603267a4641cc47
Ramon Oliveira <ramonsoliveira1@hotmail.com>
2021-01-18 20:40:16 -0300

    updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,22 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
+
+
+  // personals: any = [
+  //   {'modalidade':'Yoga Express', 'image':'yoga.jpg'},
+  //   {'modalidade':'Abdominal', 'image':'abdominal.jpg'},
+  //   {'modalidade':'Emagrecimento', 'image':'emagrecimento.jpg'},
+  //   {'modalidade':'Combat', 'image':'combat.jpg'}
+  // ];
+
+  // programas: any = [
+  //   {'modalidade':'Levantamento de Pesos', 'image':'levantamento.jpg'},
+  //   {'modalidade':'Emagrecimento', 'image':'emagrecimento.jpg'},
+  //   {'modalidade':'Combat', 'image':'combat.jpg'},
+  //   {'modalidade':'Yoga Express', 'image':'yoga.jpg'},
+  //   {'modalidade':'Abdominal', 'image':'abdominal.jpg'}
+  // ];
 
 }
--- src/pages/quem-sou/quem-sou.ts
+++ src/pages/quem-sou/quem-sou.ts
@@ -18,12 +18,21 @@ export class QuemSouPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  items: any = [
-    {'icon':'flask', 'text':'flask'},
-    {'icon':'flask', 'text':'flask'},
-    {'icon':'flask', 'text':'flask'},
-    {'icon':'flask', 'text':'flask'},
-    {'icon':'flask', 'text':'flask'}
+  infos: any = [
+    {'icon':'pin', 'text':'São José dos Campos - SP'},
+    {'icon':'school', 'text':'Técnico em Informática'},
+    {'icon':'school', 'text':'Técnico em Programação de Jogos Digitais'},
+    {'icon':'school', 'text':'Web Designer'},
+    {'icon':'school', 'text':'Analista de Sistemas'},
+    {'icon':'laptop', 'text':'Conhecimentos em IONIC, Angular, CSS, HTML, Typescript'},
+    {'icon': 'bicycle', 'text': 'Hobbies: Academia, correr, jogos, cozinhar, viajar'}
+  ];
+
+  socials: any = [
+    {'icon':'at', 'text':'ramonsoliveira1@hotmail.com', 'link': 'mailto:ramonsoliveira1@hotmail.com'},
+    {'icon':'call', 'text':'(12)98227-6505', 'link': 'tel:12982276505'},
+    {'icon':'logo-github', 'text':'https://github.com/ramou1', 'link': 'https://github.com/ramou1'},
+    {'icon':'share', 'text':'https://www.behance.net/ramou', 'link': 'https://www.behance.net/ramou'}
   ];
 
 
